c73d623fd2e2a62fdde61c8e85bc85431c040d4c
myuser <myemail>
2025-09-18 21:34:41 -0600

    testes3
    inserindo tailwind

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ function renderNotes() {
   notesContainer.innerHTML = "";
   notes.forEach(note => {
     const div = document.createElement("div");
-    div.className = "note";
+    div.className =
+      "relative p-4 rounded-xl shadow-md text-left min-h-[80px] flex flex-col justify-between transition hover:scale-[1.02]";
     div.style.backgroundColor = note.color;
     div.innerHTML = `
-      <p>${note.text}</p>
-      <button class="delete">✖</button>
+      <p class="mb-2 text-gray-800 break-words">${note.text}</p>
+      <button class="absolute top-2 right-2 bg-white/80 hover:bg-red-100 text-red-500 rounded-full p-1 transition delete" title="Excluir nota">✖</button>
     `;
     div.querySelector(".delete").addEventListener("click", () => {
       notes = notes.filter(n => n.id !== note.id);
